1de26d97f3c7667c1540b7c133770d9f34995eff
mberaha <berahamario@gmail.com>
2020-03-13 17:20:36 +0100

    ugly GraphSinkhorn distance

--- src/distance.cpp
+++ src/distance.cpp
@@ -33,6 +33,7 @@ std::tuple<arma::uvec, double> UniformSinkhorn::compute(
     dist=-10;
     call();
 
+    #pragma omp parallel for
     for (int i=0; i < n_out; i++) {
         perm(i) = transport.row(i).index_max();
     }
@@ -57,10 +58,59 @@ void UniformSinkhorn::init(const arma::mat &real_data,
 
 void UniformSinkhorn::compute_cost(const arma::mat &real_data,
                                    const arma::mat &synth_data) {
-   arma::rowvec curr;
+
+   #pragma omp parallel for
    for (int i = 0; i < n_in;  ++i) {
-       curr = real_data.row(i);
+       arma::rowvec curr = real_data.row(i);
        cost_mat.row(i) = arma::sum(
            arma::abs(synth_data.each_row() - curr), 1).t();
    }
+}
+
+std::tuple<arma::uvec, double> GraphSinkhorn::compute(
+    const std::vector<Graph> &real_data,
+    const std::vector<Graph> &synth_data)
+{
+    if (!init_done)
+        init(real_data, synth_data);
+
+    compute_cost(real_data, synth_data);
+
+    dist = -10;
+    call();
+
+#pragma omp parallel for
+    for (int i = 0; i < n_out; i++)
+    {
+        perm(i) = transport.row(i).index_max();
+    }
+    return std::make_tuple(perm, dist);
+}
+
+void GraphSinkhorn::init(const std::vector<Graph> &real_data,
+                         const std::vector<Graph> &synth_data)
+{
+    n_in = real_data.size();
+    n_out = synth_data.size();
+
+    w_in.resize(n_in);
+    w_in.fill(1.0 / n_in);
+    w_out.resize(n_in);
+    w_out.fill(1.0 / n_out);
+
+    cost_mat.resize(n_in, n_out);
+    transport.resize(n_in, n_out);
+    perm.resize(n_out);
+    init_done = true;
+}
+
+void GraphSinkhorn::compute_cost(const std::vector<Graph> &real_data,
+                                 const std::vector<Graph> &synth_data)
+{
+
+    for (int i = 0; i < n_in; ++i) {
+        for (int j = 0; j < n_out; ++j) {
+            cost_mat(i, j) = graph_dist(real_data[i], synth_data[j]);
+        }
+    }
 }
--- src/distance.hpp
+++ src/distance.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include "wasserstein.hpp"
 #include "ot/sinkhorn.hpp"
+#include "graph.hpp"
 
 class Distance {
 public:
@@ -17,10 +18,12 @@ public:
 
 class UniformDiscreteWassersteinDistance: public Distance {
 protected:
-    int max_iter = 1000;
-    double p = 1.0;
+    int max_iter;
+    int p = 1;
 
 public:
+    UniformDiscreteWassersteinDistance(int max_iter=100): max_iter(max_iter) {}
+
     std::tuple<arma::uvec, double> compute(
             const arma::mat &real_data, const arma::mat &synth_data);
 };
@@ -38,9 +41,9 @@ public:
 
 class UniformSinkhorn: public Distance {
 protected:
-    double eps = 0.1;
-    double threshold = 1e-1;
-    int max_iter = 1000;
+    double entropic_eps;
+    double threshold;
+    int max_iter;
     int p = 1;
 
     bool init_done = false;
@@ -55,17 +58,21 @@ protected:
 
 public:
 
-UniformSinkhorn(bool greedy = false): greedy(greedy) {}
+UniformSinkhorn(
+    double entropic_eps=0.1, double threshold=1e-4,
+    int max_iter=100, bool greedy = false):
+        entropic_eps(entropic_eps), threshold(threshold),
+        max_iter(max_iter), greedy(greedy) {}
 
 std::tuple<arma::uvec, double> compute(
     const arma::mat &real_data, const arma::mat &synth_data);
 
 void call() {
     if (greedy)
-        greenkhorn(w_in, w_out, cost_mat, eps, threshold, max_iter,
+        greenkhorn(w_in, w_out, cost_mat, entropic_eps, threshold, max_iter,
              p, &transport, &dist, true);
     else
-        sinkhorn(w_in, w_out, cost_mat, eps, threshold, max_iter,
+        sinkhorn(w_in, w_out, cost_mat, entropic_eps, threshold, max_iter,
              p, &transport, &dist);
 }
 
@@ -74,5 +81,51 @@ void compute_cost(const arma::mat &real_data, const arma::mat &synth_data);
 void init(const arma::mat &real_data, const arma::mat &synth_data);
 };
 
+
+class GraphSinkhorn {
+protected:
+    double entropic_eps;
+    double threshold;
+    int max_iter;
+    int p = 1;
+
+    bool init_done = false;
+    arma::vec w_in, w_out;
+    int n_in, n_out;
+    arma::mat cost_mat;
+    arma::mat transport;
+    arma::uvec perm;
+    double dist;
+
+    bool greedy = false;
+
+public:
+    GraphSinkhorn(
+        double entropic_eps = 0.1, double threshold = 1e-4,
+        int max_iter = 100, bool greedy = false):
+            entropic_eps(entropic_eps), threshold(threshold),
+            max_iter(max_iter), greedy(greedy) {}
+
+    std::tuple<arma::uvec, double> compute(
+        const std::vector<Graph> &real_data, 
+        const std::vector<Graph> &synth_data);
+
+    void call()
+    {
+        if (greedy)
+            greenkhorn(w_in, w_out, cost_mat, entropic_eps, threshold, max_iter,
+                       p, &transport, &dist, true);
+        else
+            sinkhorn(w_in, w_out, cost_mat, entropic_eps, threshold, max_iter,
+                     p, &transport, &dist);
+    }
+
+    void compute_cost(
+        const std::vector<Graph> &real_data,
+        const std::vector<Graph> &synth_data);
+
+    void init(const std::vector<Graph> &real_data,
+              const std::vector<Graph> &synth_data);
+};
 
 #endif
