84d9e4d0841551932506862f5a5527b09706554d
mberaha <berahamario@gmail.com>
2020-03-13 17:21:19 +0100

    test for the graph application

--- src/spikes/test_graph.cpp
+++ src/spikes/test_graph.cpp
@@ -1,6 +1,7 @@
 #include "../graph.hpp"
 #include "../include_arma.hpp"
-
+#include "../distance.hpp"
+#include "../abc_py_class.hpp"
 
 arma::mat generate_graph(int n_nodes, double thr) {
     arma::mat adj1(n_nodes, n_nodes, arma::fill::zeros);
@@ -23,16 +24,28 @@ int main() {
     int n_nodes = 100;
     double thr = 0.3;
 
-    std::vector<Graph> graphs;
-    graphs.push_back(Graph(generate_graph(100, 0.1)));
-    graphs.push_back(Graph(generate_graph(100, 0.15)));
-    graphs.push_back(Graph(generate_graph(100, 0.6)));
-    graphs.push_back(Graph(generate_graph(100, 0.65)));
-
-    for (int i=0; i < graphs.size(); i++) {
-        for (int j=0; j < graphs.size(); j++) {
-            std::cout << "d(g" << i << ", g" << j << ") = "
-                      << graph_dist(graphs[i], graphs[j]) << std::endl;
-        }
+    int ngraphs = 50;
+    std::vector<Graph> graphs1(ngraphs);
+    std::vector<Graph> graphs2(ngraphs);
+    std::vector<arma::mat> data(ngraphs);
+    for (int i=0; i < ngraphs; i++) {
+        arma::mat g = generate_graph(n_nodes, arma::randu() * thr);
+        data[i] = g;
+        graphs1[i] = Graph(g);
+        graphs2[ngraphs -1 - i] = Graph(g);
     }
+
+    GraphSinkhorn distance;
+    std::tuple<arma::uvec, double> dist = distance.compute(graphs1, graphs2);
+
+    std::string a = "afaa";
+    AbcPyGraph abcpy(
+        data, 1.0, 0.2, 1000, arma::mat(6, 6, arma::fill::eye),
+        arma::vec(6, arma::fill::zeros), a);
+
+    std::tuple<arma::vec, arma::mat, double> out = abcpy.run(100);
+
+    std::cout << "graph sinkhorn distance: \n"
+        << std::get<0>(dist).t() << "\n"
+        << std::get<1>(dist) << std::endl;
 }
