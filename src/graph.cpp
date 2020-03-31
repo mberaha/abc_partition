d190a4c1b9e7c23cd7ab346a6192056e3288d526
mberaha <berahamario@gmail.com>
2020-03-31 17:23:16 +0200

    new weighted distance

--- src/graph.cpp
+++ src/graph.cpp
@@ -18,10 +18,14 @@ void Graph::compute_eigenvalues()
 
 double graph_dist(const Graph &g1, const Graph &g2)
 {
+    int n1 = g1.get_n_nodes();
+    int n2 = g2.get_n_nodes();
     const arma::vec eig1 = g1.get_eigenvalues();
     const arma::vec eig2 = g2.get_eigenvalues();
+    out = arma::sum(arma::pow(eig1 - eig2, 2));
+    out *= 1.0 * (std::abs(n1 - n2) + 1.0) / (1.0 * n1 - n2);
 
-    return arma::sum(arma::pow(eig1 - eig2, 2));
+    return out;
 }
 
 arma::mat GraphSimulator::simulate_graph(arma::vec param, int n_nodes)
