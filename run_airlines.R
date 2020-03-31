9f216c7fa55094e6e88eea97d9913328a236e941
mberaha <berahamario@gmail.com>
2020-03-31 17:23:30 +0200

    better starting point for network simulations

--- run_airlines.R
+++ run_airlines.R
@@ -1,10 +1,12 @@
 rm(list=ls())
 library(ergm)
 
+networks = readRDS("airline_networks.RData")
+start_net = network(networks[[12]])
+
 simulate_ergm <- function(theta, nnodes=100) {
-  y = network(nnodes, directed=F) 
   as.matrix(simulate(
-    y ~ degree(0) + degree(1) + degree(10) + degree(50) + degree(70), 
+    start_net ~ edges + degree(0) + degree(1) + degree(10) + degree(50) + degree(70), 
     coef=theta, burnin=100, interval=100))
 }
 
@@ -13,13 +15,15 @@ library(devtools)
 compileAttributes(pkgdir = ".", verbose = getOption("verbose"))
 load_all(reset=T, recompile=T)
 
-networks = readRDS("airline_networks.RData")
 
-var_chol = diag(1, 5, 5) * 500
-m0 = rep(0, 5)
+
+var_chol = diag(1, 6, 6) * 30
+m0 = c(-10, -40, 0, 0, -100, -20)
 eps = 30
 
-inits = list(c(50, 60, -1000, -1000, 22), c(0, 0, 50, 10, 22))
+inits = list(c(-10, 100, 100, -100, -100, -100), 
+             c(-37, -11, -11, -14, -100, 63),
+             c(1, 15, 3, 100, 100, -100))
 
 out = runAbcMCMC_graph(networks, 100000, 1, 0.1, m0, var_chol, inits, eps, "aaaa")
 
